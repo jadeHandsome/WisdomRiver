854b8121d2ddc324fe6198ec8f22120ab15b837b
拿把破菜刀 <1149769831@qq.com>
2017-12-19 14:46:12 +0800

    no message

---
+++ WisdomRiver/Classes/Mine/InfoManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoManagerViewController.h
+//  WisdomRiver
+//
+//  Created by 曾洪磊 on 2017/12/19.
+//  Copyright © 2017年 曾洪磊. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface InfoManagerViewController : BaseViewController
+
+@end
