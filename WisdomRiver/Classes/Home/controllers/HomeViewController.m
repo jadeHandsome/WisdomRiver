144553da4e51e83f1d705c3ebdd1b1ee932bd5f8
周春仕 <350499088@qq.com>
2017-12-28 15:03:00 +0800

    no message

--- WisdomRiver/Classes/Home/controllers/HomeViewController.m
+++ WisdomRiver/Classes/Home/controllers/HomeViewController.m
@@ -190,10 +190,10 @@
     [self.view addSubview:topImage];
     [topImage mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.top.right.equalTo(self.view);
-        make.height.mas_equalTo(HEIGHT(566) - 64 + navHight);
+        make.height.mas_equalTo(HEIGHT(500) - 64 + navHight);
     }];
     
-    _pageFlowView = [[NewPagedFlowView alloc] initWithFrame:CGRectMake(0, 0, SIZEWIDTH, HEIGHT(566) - 64 + navHight)];
+    _pageFlowView = [[NewPagedFlowView alloc] initWithFrame:CGRectMake(0, 0, SIZEWIDTH, HEIGHT(500) - 64 + navHight)];
     _pageFlowView.delegate = self;
     _pageFlowView.dataSource = self;
     _pageFlowView.minimumPageAlpha = 1;
@@ -203,7 +203,7 @@
     _pageFlowView.autoTime = 3.0;
     
     //初始化pageControl
-    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0,(HEIGHT(896) - 64 + navHight) / 2 , SIZEWIDTH, (HEIGHT(236) - 64 + navHight) / 2)];
+    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0,(HEIGHT(800) - 64 + navHight) / 2 , SIZEWIDTH, (HEIGHT(200) - 64 + navHight) / 2)];
     _pageFlowView.pageControl = pageControl;
     [_pageFlowView addSubview:pageControl];
     pageControl.alpha = 1;
@@ -214,7 +214,7 @@
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
     [layout setScrollDirection:UICollectionViewScrollDirectionVertical];
 
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0,HEIGHT(586) - 64 + navHight , SIZEWIDTH, SIZEHEIGHT - (HEIGHT(586) - 64 + navHight) - self.tabBarController.tabBar.frame.size.height) collectionViewLayout:layout];
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0,HEIGHT(520) - 64 + navHight , SIZEWIDTH, SIZEHEIGHT - (HEIGHT(586) - 64 + navHight) - self.tabBarController.tabBar.frame.size.height) collectionViewLayout:layout];
     collectionView.backgroundColor = [UIColor whiteColor];
     collectionView.delegate = self;
     collectionView.dataSource = self;
@@ -256,7 +256,7 @@
         UILabel *today = [[UILabel alloc] init];
         today.text = @"今天(限行)";
         today.textColor = [UIColor whiteColor];
-        today.font = [UIFont systemFontOfSize:HEIGHT(55)];
+        today.font = [UIFont systemFontOfSize:17];
         [leftView addSubview:today];
         [today mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(leftView).offset(HEIGHT(50));
@@ -283,7 +283,7 @@
             [leftView addSubview:todayText1];
             [todayText1 mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.bottom.equalTo(leftView).offset(HEIGHT(-25));
-                make.height.mas_equalTo(HEIGHT(143));
+                make.height.mas_equalTo(HEIGHT(120));
                 make.width.mas_equalTo(WIDTH(100));
                 make.right.equalTo(leftText.mas_left).offset(WIDTH(-25));
             }];
@@ -298,7 +298,7 @@
             [leftView addSubview:todayText2];
             [todayText2 mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.bottom.equalTo(leftView).offset(HEIGHT(-25));
-                make.height.mas_equalTo(HEIGHT(143));
+                make.height.mas_equalTo(HEIGHT(120));
                 make.width.mas_equalTo(WIDTH(100));
                 make.left.equalTo(leftText.mas_right).offset(WIDTH(25));
             }];
@@ -307,7 +307,7 @@
             UILabel *leftText = [[UILabel alloc] init];
             leftText.text = @"不限行";
             leftText.textColor = [UIColor whiteColor];
-            leftText.font = [UIFont systemFontOfSize:HEIGHT(55)];
+            leftText.font = [UIFont systemFontOfSize:17];
             [leftView addSubview:leftText];
             [leftText mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.bottom.equalTo(leftView).offset(HEIGHT(-80));
@@ -324,7 +324,7 @@
         UILabel *nextDay = [[UILabel alloc] init];
         nextDay.text = @"明天(限行)";
         nextDay.textColor = [UIColor whiteColor];
-        nextDay.font = [UIFont systemFontOfSize:HEIGHT(55)];
+        nextDay.font = [UIFont systemFontOfSize:17];
         [rightView addSubview:nextDay];
         [nextDay mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(rightView).offset(HEIGHT(50));
@@ -351,7 +351,7 @@
             [rightView addSubview:nextDayText1];
             [nextDayText1 mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.bottom.equalTo(rightView).offset(HEIGHT(-25));
-                make.height.mas_equalTo(HEIGHT(143));
+                make.height.mas_equalTo(HEIGHT(120));
                 make.width.mas_equalTo(WIDTH(100));
                 make.right.equalTo(rightText.mas_left).offset(WIDTH(-25));
             }];
@@ -366,7 +366,7 @@
             [rightView addSubview:nextDatText2];
             [nextDatText2 mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.bottom.equalTo(rightView).offset(HEIGHT(-25));
-                make.height.mas_equalTo(HEIGHT(143));
+                make.height.mas_equalTo(HEIGHT(120));
                 make.width.mas_equalTo(WIDTH(100));
                 make.left.equalTo(rightText.mas_right).offset(WIDTH(25));
             }];
@@ -375,7 +375,7 @@
             UILabel *rightText = [[UILabel alloc] init];
             rightText.text = @"不限行";
             rightText.textColor = [UIColor whiteColor];
-            rightText.font = [UIFont systemFontOfSize:HEIGHT(55)];
+            rightText.font = [UIFont systemFontOfSize:17];
             [rightView addSubview:rightText];
             [rightText mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.bottom.equalTo(rightView).offset(HEIGHT(-80));
@@ -408,8 +408,12 @@
             make.left.equalTo(imageView.mas_right).offset(WIDTH(45));
             make.centerY.equalTo(topView.mas_centerY);
         }];
+        NSDate *date = [NSDate date];
+        NSDateFormatter *objDateformat = [[NSDateFormatter alloc] init];
+        [objDateformat setDateFormat:@"MM月dd日"];
+        NSString * timeStr = [NSString stringWithFormat:@"%@",[objDateformat stringFromDate: date]];
         UILabel *dateLabel = [[UILabel alloc] init];
-        dateLabel.text = @"12月15日";
+        dateLabel.text = timeStr;
         dateLabel.textColor = [UIColor whiteColor];
         dateLabel.font = [UIFont systemFontOfSize:HEIGHT(42)];
         [topView addSubview:dateLabel];
@@ -469,7 +473,7 @@
 }
 
 - (CGSize)sizeForPageInFlowView:(NewPagedFlowView *)flowView {
-    return CGSizeMake(WIDTH(780), HEIGHT(330));
+    return CGSizeMake(WIDTH(780), HEIGHT(300));
 }
 
 #pragma mark ------ UICollectionView -------------------
